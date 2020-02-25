8323a0af177bba842fe1d315eaa6c96f9c9a6203
Alekseev Aleksandr <alekseev2002@inbox.ru>
2020-02-25 23:39:07 +0300

    Moved field to namespace

--- include/PreciseArithmetics/field.h
+++ include/PreciseArithmetics/field.h
@@ -1,9 +1,11 @@
 #ifndef PA_FIELD_H
 #define PA_FIELD_H
 
-class Field {
-public:
-    static const unsigned int Char = 0;
-};
+namespace pa {
+	class Field {
+	public:
+	    static const unsigned int Char = 0;
+	};
+}
 
 #endif //PA_FIELD_H
